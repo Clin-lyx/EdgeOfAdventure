ed4828e2cf8572286f75b62d1577097eb3a9c73b
Clin-lyx <110957868+Clin-lyx@users.noreply.github.com>
2023-07-01 10:38:12 +0800

    New font, main menu under implementation.

---
+++ EdgeOfAdventure/Assets/Scripts/MainMenu/Scrolling.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scrolling : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private RawImage _img;
+    [SerializeField] private float _x;
+    private bool moveLeft = true;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!moveLeft) {
+            _img.uvRect = new Rect(_img.uvRect.position - new Vector2(_x, 0f) * Time.deltaTime, _img.uvRect.size);
+            if (_img.uvRect.x <= 0f) {
+                moveLeft = true;
+            }
+        } else {
+            _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x, 0f) * Time.deltaTime, _img.uvRect.size);
+            if (_img.uvRect.x >= 0.08f) {
+                moveLeft = false;
+            }
+        }
+    }
+}
