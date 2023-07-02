7ad00e1e63aa8d5c2cd3a47b964d24a1a79c0205
Clin-lyx <110957868+Clin-lyx@users.noreply.github.com>
2023-07-03 00:46:15 +0800

    Basic Pause Menu

--- EdgeOfAdventure/Assets/Scripts/Audio/AudioManager.cs
+++ EdgeOfAdventure/Assets/Scripts/Audio/AudioManager.cs
@@ -9,21 +9,44 @@ public class AudioManager : MonoBehaviour
     [Header("Event listeners")]
     public PlayAudioEventSO FXEvent;
     public PlayAudioEventSO BGMEvent;
+    public FloatEventSO volumeEvent;
+    public VoidEventSO pauseEvent;
+
+    [Header("Broadcast")]
+    public FloatEventSO syncVolumeEvent;
 
     [Header("Components")]
     public AudioSource FXSource;
     public AudioSource BGMSource;
+    public AudioMixer mixer;
 
     private void OnEnable()
     {
         FXEvent.OnEventRaised += OnFXEvent;
         BGMEvent.OnEventRaised += OnBGMEvent;
+        volumeEvent.OnEventRaised += OnVolumeEvent;
+        pauseEvent.OnEventRaised += onPauseEvent;
     }
 
     private void OnDisable()
     {
         FXEvent.OnEventRaised -= OnFXEvent;
         BGMEvent.OnEventRaised -= OnBGMEvent;
+        volumeEvent.OnEventRaised -= OnVolumeEvent;
+        pauseEvent.OnEventRaised -= onPauseEvent;
+    }
+
+    private void onPauseEvent()
+    {   
+        float amount;
+        mixer.GetFloat("MasterVolume", out amount);
+
+        syncVolumeEvent.RaiseEvent(amount);
+    }
+
+    private void OnVolumeEvent(float amount)
+    {
+        mixer.SetFloat("MasterVolume", amount * 100 - 80);
     }
 
     private void OnBGMEvent(AudioClip clip)
--- EdgeOfAdventure/Assets/Scripts/UI/UIManager.cs
+++ EdgeOfAdventure/Assets/Scripts/UI/UIManager.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -9,20 +11,51 @@ public class UIManager : MonoBehaviour
 
     [Header("Event listeners")]
     public CharacterEventSO healthEvent;
+    public FloatEventSO syncVolumeEvent;
+
+    [Header("Component")]
+    public Button SettingsBtn;
+    public GameObject pausePanel;
+    public Slider volumeSlider;
+
+    [Header("Broadcast")]
+    public VoidEventSO pauseEvent;
+
+    private void Awake() {
+        SettingsBtn.onClick.AddListener(TogglePausePanel);
+    }
 
     private void OnEnable()
     {
         healthEvent.OnEventRaised += OnHealthEvent;
+        syncVolumeEvent.OnEventRaised += OnSyncVolumeEvent;
     }
 
     private void OnDisable()
     {
         healthEvent.OnEventRaised -= OnHealthEvent;
+        syncVolumeEvent.OnEventRaised -= OnSyncVolumeEvent;
+    }
+
+    private void OnSyncVolumeEvent(float amount)
+    {
+        volumeSlider.value = (amount + 80) / 100;
     }
 
     private void OnHealthEvent(Character character)
     {
         var persentage = character.currentHealth / character.maxHealth;
         playerStatBar.OnHealthChange(persentage);
+    }
+
+    private void TogglePausePanel() {
+        if(pausePanel.activeInHierarchy) {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1;
+        } else {
+            pauseEvent.RaiseEvent();
+            pausePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 }
