fc7a0a893f96a703875c268e37f57e5ee579a045
NusMinato <E0985934@u.nus.edu>
2023-07-22 20:12:54 +0800

    Package with bugs

--- EdgeOfAdventure/Assets/Scripts/Transition/InitialLoad.cs
+++ EdgeOfAdventure/Assets/Scripts/Transition/InitialLoad.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 public class InitialLoad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AssetReference persistentScene;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        Addressables.LoadSceneAsync(persistentScene);
     }
 }
